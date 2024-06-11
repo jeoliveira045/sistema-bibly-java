470b73a6429a99d238d13abfce9c7b8681b633f6
jeoliveira045 <jean.mateus.1997@gmail.com>
2024-06-11 17:50:27 -0300

    Docker compose, application.yml e massa de dados de livros e solicitantes

---
+++ src/main/resources/docker/massa_dados_livros.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO LIVROS (NOME, ISBN, GENERO, AUTOR) VALUES ('O Senhor dos Anéis', '978-3-16-148410-0', 'Fantasia', 'J.R.R. Tolkien');
+INSERT INTO LIVROS (NOME, ISBN, GENERO, AUTOR) VALUES ('1984', '978-0-452-28423-4', 'Distopia', 'George Orwell');
+INSERT INTO LIVROS (NOME, ISBN, GENERO, AUTOR) VALUES ('Dom Casmurro', '978-0-14-044745-8', 'Clássico', 'Machado de Assis');
+INSERT INTO LIVROS (NOME, ISBN, GENERO, AUTOR) VALUES ('O Alquimista', '978-0-06-112241-5', 'Ficção', 'Paulo Coelho');
+INSERT INTO LIVROS (NOME, ISBN, GENERO, AUTOR) VALUES ('A Menina que Roubava Livros', '978-0-375-84220-7', 'Drama', 'Markus Zusak');
+INSERT INTO LIVROS (NOME, ISBN, GENERO, AUTOR) VALUES ('Cem Anos de Solidão', '978-0-06-088328-7', 'Realismo Mágico', 'Gabriel García Márquez');
+INSERT INTO LIVROS (NOME, ISBN, GENERO, AUTOR) VALUES ('Harry Potter e a Pedra Filosofal', '978-0-590-35340-3', 'Fantasia', 'J.K. Rowling');
+INSERT INTO LIVROS (NOME, ISBN, GENERO, AUTOR) VALUES ('O Pequeno Príncipe', '978-2-07-061275-8', 'Fábula', 'Antoine de Saint-Exupéry');
+INSERT INTO LIVROS (NOME, ISBN, GENERO, AUTOR) VALUES ('Orgulho e Preconceito', '978-0-19-953556-9', 'Romance', 'Jane Austen');
+INSERT INTO LIVROS (NOME, ISBN, GENERO, AUTOR) VALUES ('Moby Dick', '978-0-14-243724-7', 'Aventura', 'Herman Melville');
