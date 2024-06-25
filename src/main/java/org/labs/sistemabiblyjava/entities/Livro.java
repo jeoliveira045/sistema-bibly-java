cebbc168682852a7f11a6cc520eb123df275aa93
jean.oliveira <jean.oliveira@tre-pa.jus.br>
2024-06-25 12:45:48 -0300

    feat: Criação das classes Editora e Colecao #5 closes #5

--- src/main/java/org/labs/sistemabiblyjava/entities/Livro.java
+++ src/main/java/org/labs/sistemabiblyjava/entities/Livro.java
@@ -1,8 +1,16 @@
 package org.labs.sistemabiblyjava.entities;
 
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.ManyToAny;
+import org.labs.sistemabiblyjava.entities.databind.ColecaoDatabind;
+import org.labs.sistemabiblyjava.entities.databind.EditoraDatabind;
+import org.labs.sistemabiblyjava.entities.databind.SituacaoEmprestimoDatabind;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "livros")
@@ -19,4 +27,28 @@ public class Livro {
     private String genero;
     @Column(name = "autor")
     private String autor;
+
+    @Column(name = "altura")
+    private Double altura;
+
+    @Column(name = "largura")
+    private Double largura;
+
+    @Column(name = "edicao")
+    private String edicao;
+
+    @Column(name = "dataedicao")
+    private LocalDate dataEdicao;
+
+    @ManyToOne
+    @JsonSerialize(using = EditoraDatabind.IdSerializer.class)
+    @JsonDeserialize(using = EditoraDatabind.IdDeserializer.class)
+    private Editora editora;
+
+    @ManyToOne
+    @JsonSerialize(using = ColecaoDatabind.IdSerializer.class)
+    @JsonDeserialize(using = ColecaoDatabind.IdDeserializer.class)
+    private Colecao colecao;
+
+
 }
